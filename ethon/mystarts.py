fa1c976ed2aabaf8ea667e14ae91a46e8d9c29ad
sofyiq <150921026+sofyiq@users.noreply.github.com>
2023-12-09 10:15:07 +0300

    Update mystarts.py

--- ethon/mystarts.py
+++ ethon/mystarts.py
@@ -8,7 +8,7 @@ async def start_srb(event, st):
                       buttons=[
                               [Button.inline("SET THUMB.", data="set"),
                                Button.inline("REM THUMB.", data="rem")],
-                              [Button.url("DEV", url="t.me/MaheshChauhan")]])
+                              [Button.url("DEV", url="t.me/SoFe_Iraq")]])
                               
     
 async def vc_menu(event):
@@ -18,5 +18,5 @@ async def vc_menu(event):
                          Button.inline("SOURCE", data="source")],
                         [Button.inline("NOTICE.", data="notice"),
                          Button.inline("Main.", data="help")],
-                        [Button.url("DEVELOPER", url="t.me/MaheshChauhan")]])
+                        [Button.url("DEVELOPER", url="t.me/SoFe_Iraq")]])
     
